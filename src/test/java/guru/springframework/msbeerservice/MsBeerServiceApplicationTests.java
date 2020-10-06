0faabf4a1325922be2912e7f13b6d6c5a3593f2d
Nivaldo Monteiro <nivaldomonteiro@Nivaldos-MBP.home>
2020-10-06 21:49:54 +0100

    Added the base springboot project.

---
+++ src/test/java/guru/springframework/msbeerservice/MsBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
