0865b670ea5b51098e02146c6446e15d83647217
nivaldomonteiro <nushnivaldo@gmail.com>
2020-10-15 21:43:16 +0100

    Added jpa repository for beer - closes #4

---
+++ src/main/java/guru/springframework/msbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.msbeerservice.repositories;
+
+import guru.springframework.msbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
