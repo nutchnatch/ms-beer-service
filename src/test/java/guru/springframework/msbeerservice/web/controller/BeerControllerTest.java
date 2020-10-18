0cb2d2cdea06dd5eda63252d1f701291f0250a92
nivaldomonteiro <nushnivaldo@gmail.com>
2020-10-18 03:03:31 +0100

    Fixed unit test

--- src/test/java/guru/springframework/msbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/msbeerservice/web/controller/BeerControllerTest.java
@@ -2,13 +2,16 @@ package guru.springframework.msbeerservice.web.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.msbeerservice.domain.Beer;
 import guru.springframework.msbeerservice.web.model.BeerDto;
+import guru.springframework.msbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,7 +36,7 @@ class BeerControllerTest {
     @Test
     void saveNewBear() throws Exception {
 
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidBeerDto();
         String builderDtoJson = objectMapper.writeValueAsString(beerDto);
         mockMvc.perform(post("/api/v1/beer/")
                 .contentType(MediaType.APPLICATION_JSON).content(builderDtoJson))
@@ -42,11 +45,20 @@ class BeerControllerTest {
 
     @Test
     void updateBeerById() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
         mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
                 .andExpect(status().isNoContent());
+    }
+
+    private BeerDto getValidBeerDto() {
+        return BeerDto.builder()
+                .beerName("My Beer")
+                .beerStyle(BeerStyleEnum.ALE)
+                .price(new BigDecimal("2.99"))
+                .upc(1212324324342L)
+                .build();
     }
 }
